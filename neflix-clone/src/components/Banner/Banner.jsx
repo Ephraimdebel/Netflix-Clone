23b5260dc00468fd048f0a6117d0b75a51caaf8e
Ephraimdebel <ephraimdebel@gmail.com>
2024-09-04 02:45:53 -0700

    banner added

---
+++ neflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from "../../utils/axios";
+import requests from '../../utils/requests';
+import "./banner.css"
+function Banner() {
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                const randomIndex = Math.floor(Math.random() * request.data.results.length);
+                setMovie(request.data.results[randomIndex]);
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    const truncateText = (text, maxLength) => {
+        return text?.length > maxLength ? text.substring(0, maxLength-1) + '...' : text;
+    };
+
+    return (
+        <div>
+
+        
+        <div className='banner' style={{
+            backgroundSize: "cover",
+            backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat"
+        }}>
+            <div className='banner__contents'>
+                <h1 className='banner__title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <div className='banner__buttons'>
+                    <button className='banner__buttons play'>Play</button>
+                    <button className='banner__buttons'>My List</button>
+                </div>
+                {/* <h1 className='banner_description'>{substring(movie?.overview,150)}</h1>  */}
+                <h1 className='banner_description'>{truncateText(movie?.overview,150)}</h1>
+            </div>
+            <div className='banner__fadeBottom' />
+        </div>
+        </div>
+    );
+}
+
+export default Banner;
--- neflix-clone/src/utils/axios.js
+++ neflix-clone/src/utils/axios.js
@@ -5,3 +5,6 @@ const instance = axios.create({
 });
 
 export default instance;
+
+
+// https://api.themoviedb.org/3/discover/tv?api_key=bc2f9a5a312555ea300e2edc245d583b&with_networks=213
