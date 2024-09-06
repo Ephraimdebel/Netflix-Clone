6d4cb53dd5f0ddc8076f6e94a1795e15fadad2eb
Ephraimdebel <ephraimdebel@gmail.com>
2024-09-06 04:22:16 -0700

    github workflow

--- neflix-clone/vite.config.js
+++ neflix-clone/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import ViteGhPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA(),
-    ViteGhPages({
-      root: 'dist',
-      build: true
-    })
-  ]
+  plugins: [react()],
+    base: '/Netflix-Clone/'
+  
 });
